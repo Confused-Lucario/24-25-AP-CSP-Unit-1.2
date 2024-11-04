57abe4db7bb3650d440667f74716d2340290f593
The Cat <philip.birkland@vtsd.com>
2024-11-04 10:46:53 -0500

    Philip Birkland unit 1.2.1 steps 1-50 (Finished)

--- 1-2-2/starter.py
+++ 1-2-2/starter.py
@@ -8,8 +8,7 @@
 # -----import statements-----
 import turtle as trtl
 import random as rand
-import leaderboard as lb
-
+importt leaderboard as lb
 # -----game configuration-----
 # To view in trinket change the values of font_size, spot_size, and
 # screen_size by half
@@ -110,22 +109,6 @@ def start_game():
     spot.onclick(spot_clicked)
     counter.getscreen().ontimer(countdown, counter_interval)
 
-def manage_leaderboard():
-
-  global score
-  global spot
-
-  # get the names and scores from the leaderboard file
-  leader_names_list = lb.get_names(leaderboard_file_name)
-  leader_scores_list = lb.get_scores(leaderboard_file_name)
-
-  # show the leaderboard with or without the current player
-  if (len(leader_scores_list) < 5 or score >= leader_scores_list[4]):
-    lb.update_leaderboard(leaderboard_file_name, leader_names_list, leader_scores_list, player_name, score)
-    lb.draw_leaderboard(True, leader_names_list, leader_scores_list, spot, score)
-
-  else:
-    lb.draw_leaderboard(False, leader_names_list, leader_scores_list, spot, score)
 
 # ----------events----------
 start_game()
