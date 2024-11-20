2ce9057ee61d71484179dcdf1e705efe4bfd4aba
The Cat <philip.birkland@vtsd.com>
2024-11-20 13:45:12 -0500

    Philip Birkland unit 1.2.3
    steps FINAL

--- 1-2-3/main.py
+++ 1-2-3/main.py
@@ -16,7 +16,8 @@ apple_image = "apple.gif" # Store the file name of your shape
 ground_height = -200
 apple_letter_x_offset = -25
 apple_letter_y_offset = -50
-letter = ""
+letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+apple_letters = []
 
 wn = trtl.Screen()
 wn.setup(width=1.0, height=1.0)
@@ -31,10 +32,11 @@ wn.tracer(False)
 # by the image file
 def draw_apple(apple):
   apple.shape(apple_image)
-  global letter
-  letter = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+  global letters
+  letter = letters.pop(len(letters) - 1)
+  apple_letters.append(letter)
   apple.goto(rd.randint(-150, 150), rd.randint(0, 100))
-  draw_letter(letter.pop(rd.randint(0, len(letter))), apple)
+  draw_letter(letter, apple)
   apple.showturtle()
   wn.update()
 
@@ -59,8 +61,138 @@ def draw_letter(letter, active_apple):
   active_apple.setpos(remember_position)
   wn.update()
 
+def check_apple_a():
+  if ("a" in apple_letters):
+    drop_apple()
+
+def check_apple_b():
+  if ("b" in apple_letters):
+    drop_apple()
+
+def check_apple_c():
+  if ("c" in apple_letters):
+    drop_apple()
+
+def check_apple_d():
+  if ("d" in apple_letters):
+    drop_apple()
+
+def check_apple_e():
+  if ("e" in apple_letters):
+    drop_apple()
+
+def check_apple_f():
+  if ("f" in apple_letters):
+    drop_apple()
+
+def check_apple_g():
+  if ("g" in apple_letters):
+    drop_apple()
+
+def check_apple_h():
+  if ("h" in apple_letters):
+    drop_apple()
+
+def check_apple_i():
+  if ("i" in apple_letters):
+    drop_apple()
+
+def check_apple_j():
+  if ("j" in apple_letters):
+    drop_apple()
+
+def check_apple_k():
+  if ("k" in apple_letters):
+    drop_apple()
+
+def check_apple_l():
+  if ("l" in apple_letters):
+    drop_apple()
+
+def check_apple_m():
+  if ("m" in apple_letters):
+    drop_apple()
+
+def check_apple_n():
+  if ("n" in apple_letters):
+    drop_apple()
+
+def check_apple_o():
+  if ("o" in apple_letters):
+    drop_apple()
+
+def check_apple_p():
+  if ("p" in apple_letters):
+    drop_apple()
+
+def check_apple_q():
+  if ("q" in apple_letters):
+    drop_apple()
+
+def check_apple_r():
+  if ("r" in apple_letters):
+    drop_apple()
+
+def check_apple_s():
+  if ("s" in apple_letters):
+    drop_apple()
+
+def check_apple_t():
+  if ("t" in apple_letters):
+    drop_apple()
+
+def check_apple_u():
+  if ("u" in apple_letters):
+    drop_apple()
+
+def check_apple_v():
+  if ("v" in apple_letters):
+    drop_apple()
+
+def check_apple_w():
+  if ("w" in apple_letters):
+    drop_apple()
+
+def check_apple_x():
+  if ("x" in apple_letters):
+    drop_apple()
+
+def check_apple_y():
+  if ("y" in apple_letters):
+    drop_apple()
+
+def check_apple_z():
+  if ("z" in apple_letters):
+    drop_apple()
+
 draw_apple(apple)
-wn.onkeypress(drop_apple, "a")
+wn.onkeypress(check_apple_a, "a")
+wn.onkeypress(check_apple_b, "b")
+wn.onkeypress(check_apple_c, "c")
+wn.onkeypress(check_apple_d, "d")
+wn.onkeypress(check_apple_e, "e")
+wn.onkeypress(check_apple_f, "f")
+wn.onkeypress(check_apple_g, "g")
+wn.onkeypress(check_apple_h, "h")
+wn.onkeypress(check_apple_i, "i")
+wn.onkeypress(check_apple_j, "j")
+wn.onkeypress(check_apple_k, "k")
+wn.onkeypress(check_apple_l, "l")
+wn.onkeypress(check_apple_m, "m")
+wn.onkeypress(check_apple_n, "n")
+wn.onkeypress(check_apple_o, "o")
+wn.onkeypress(check_apple_o, "p")
+wn.onkeypress(check_apple_q, "q")
+wn.onkeypress(check_apple_r, "r")
+wn.onkeypress(check_apple_s, "s")
+wn.onkeypress(check_apple_t, "t")
+wn.onkeypress(check_apple_u, "u")
+wn.onkeypress(check_apple_v, "v")
+wn.onkeypress(check_apple_w, "w")
+wn.onkeypress(check_apple_x, "x")
+wn.onkeypress(check_apple_y, "y")
+wn.onkeypress(check_apple_z, "z")
+
 
 wn.listen()
 trtl.mainloop()
