c9b9569e550e241200f15ea5df93784530431aab
The Cat <philip.birkland@vtsd.com>
2024-12-06 12:54:57 -0500

    Philip Birkland unit 1.2.4 Complete

--- 1-2-4/maze_main.py
+++ 1-2-4/maze_main.py
@@ -6,7 +6,8 @@ wn = trtl.Screen()
 maze_painter = trtl.Turtle()
 length = 35
 path_width = 30
-
+barrier_rand = 0
+door_space = 0
 
 # Setup Turtle
 maze_painter.left(90)
@@ -20,36 +21,25 @@ maze_painter.speed(0)
 # Increment Length
 # Repeat
 def draw_barrier():
-    barrier = rd.randint(path_width * 2, length - path_width)
-    maze_painter.forward(barrier - barrier)
     maze_painter.right(90)
     maze_painter.forward(path_width)
     maze_painter.backward(path_width)
     maze_painter.left(90)
 
-
 for wall in range(21):
-    door = rd.randint(path_width * 2, length + (path_width * 2))
-    maze_painter.forward(length/3)
+    door = rd.randint(0, length - path_width)
+    maze_painter.forward(door)
     maze_painter.penup()
     maze_painter.forward(path_width)
     maze_painter.pendown()
     if wall > 5:
+        door_space = rd.randint(0, length - path_width - door)
+        maze_painter.forward(door_space)
         draw_barrier()
 
-    maze_painter.forward(length - path_width - (length / 3))
+    maze_painter.forward(length - door - path_width - door_space)
     maze_painter.left(90)
     length += 15
-
-
-
-
-barrier = rd.randint(path_width * 2, (length - path_width * 2))
-
-
-
-
-
 
 wn.listen()
 wn.mainloop()
