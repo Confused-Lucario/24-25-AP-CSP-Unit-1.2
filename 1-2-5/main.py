5dc4ff0ac19a463500e33853be1e1fb8b1babdc9
The Cat <philip.birkland@vtsd.com>
2024-12-12 13:11:26 -0500

    Philip Birkland unit 1.2.4 Complete

--- 1-2-5/main.py
+++ 1-2-5/main.py
@@ -19,21 +19,84 @@ ball = trtl.Turtle()
 player1_score = trtl.Turtle()
 player2_score = trtl.Turtle()
 
+# game parameters
+game_width = 700
+game_height = 400
+ball_size = 50
+
 # Setup Turtle
-wn.screensize(900, 900)
+wn.screensize(game_width + 200, game_height + 200)
 wn.bgcolor('black')
 player1.pencolor('white')
 player2.pencolor('white')
 player1_score.pencolor('white')
 player2_score.pencolor('white')
 game_wall.pencolor('white')
+game_wall.speed('fastest')
+game_wall.pensize(5)
+game_wall.hideturtle()
 ball.pencolor('white')
+ball.speed(1)
+ball.penup()
+
+# define functions
+def draw_game_boarder():
+    game_wall.penup()
+    game_wall.goto(game_width/2, game_height/2)
+    game_wall.pendown()
+    game_wall.goto(game_width/2, game_height/-2)
+    game_wall.goto(game_width/-2, game_height/-2)
+    game_wall.goto(game_width/-2, game_height/2)
+    game_wall.goto(game_width/2, game_height/2)
+
+def launch_ball():
+    ball.setheading(rd.randint(0, 360))
 
 
 
+def ball_bounce_y():
+    if ball.heading() > 0 and ball.heading() < 90:
+        ball.setheading(ball.heading() - 90)
+        ball.forward(ball_size)
+    elif ball.heading() > 90 and ball.heading() < 180:
+        ball.setheading(ball.heading() + 90)
+        ball.forward(ball_size)
+    elif ball.heading() > 180 and ball.heading() < 270:
+        ball.setheading(ball.heading() - 90)
+        ball.forward(ball_size)
+    elif ball.heading() > 270 and ball.heading() < 360:
+        ball.setheading(ball.heading() + 90)
+        ball.forward(ball_size)
+
+def ball_bounce_x():
+    if ball.heading() > 0 and ball.heading() < 90:
+        ball.setheading(ball.heading() + 90)
+        ball.forward(ball_size)
+    elif ball.heading() > 90 and ball.heading() < 180:
+        ball.setheading(ball.heading() - 90)
+        ball.forward(ball_size)
+    elif ball.heading() > 180 and ball.heading() < 270:
+        ball.setheading(ball.heading() + 90)
+        ball.forward(ball_size)
+    elif ball.heading() > 270 and ball.heading() < 360:
+        ball.setheading(ball.heading() - 90)
+        ball.forward(ball_size)
 
 
+# game code
+draw_game_boarder()
 
+launch_ball()
+
+while True:
+    if ball.ycor() + 50 > game_height/2 or ball.ycor() - 50 < game_height/-2:
+        ball_bounce_y()
+
+    if ball.xcor() + 50 > game_width/2 or ball.xcor() - 50 < game_width/-2:
+        ball_bounce_x()
+
+    else:
+        ball.forward(10)
 
 # loop to keep window open
 wn.listen()
